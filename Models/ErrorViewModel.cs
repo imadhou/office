88721235656e0b15a5519e0efe058c3dfe02d231
imadhou <imadhou00@gmail.com>
2021-08-10 14:49:11 +0200

    project init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace office.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
